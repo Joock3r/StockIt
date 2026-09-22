f6f976c1236d1e6c0ed6d102425818ff90fbe652
Joock3r <tamirr.ts@outlook.com>
2026-09-22 12:49:04 +0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -2,11 +2,8 @@ const CACHE_NAME = 'shopping-app-shell-v1';
 const APP_SHELL = [
   './index.html',
   './manifest.json',
-  './icons/icon-192.png',
-  './icons/icon-512.png',
-  './icons/icon-180.png',
-  './icons/icon-32.png',
-  './icons/icon-16.png'
+  './icon-192.png',
+  './icon-512.png'
 ];
 
 self.addEventListener('install', (event) => {
